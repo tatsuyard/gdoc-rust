8d6822f72d4fe5aa984b7237b13ebbbc102e1972
TatsuyaKamikawara <tatsuyardful@gmail.com>
2021-01-26 19:18:44 +0900

    tmp

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
-use structopt::StructOpt;
+use anyhow::{Context, Result};
 
-#[derive(StructOpt)]
-struct Cli {
-    pattern: String,
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
-
-fn main() {
-    let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+fn main() -> Result<()> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path).with_context(|| format!("could not read file `{}`", path))?;
+    println!("file content: {}", content);
+    Ok(())
 }
