1f7e0ea9ee5b8f13a03f21593c7c0f7b6a0fad08
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-03-23 07:01:18 +0900

    可変の参照

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+    }
+    let r2 = &mut s;
   
-    change(&mut s);
-    println!("{}", s);
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
-}
+
