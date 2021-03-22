4f100aa2ed4f9390896a3018c6a24f957ac9e1d1
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-03-23 07:30:11 +0900

    tmp

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
-    let mut s = String::from("hello");
-    {
-        let r1 = &mut s;
-    }
-    let r2 = &mut s;
-  
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // wordの中身は、値5になる
+
+    s.clear(); // Stringを空にする。つまり、""と等しくする  
 }
 
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
 
