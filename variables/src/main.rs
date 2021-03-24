82b080e62549aa3b685b711206a9d35b5f42adea
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-03-25 06:57:40 +0900

    構造体

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,24 +1,17 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let my_string = String::from("hello world");
+    let rect1 = Rectangle { width: 30, height: 50 };
 
-    let word = first_word(&my_string[..]);
-
-    let my_string_literal = "hello world";
-
-    let word = first_word(&my_string_literal[..]);
-
-    let word = first_word(my_string_literal);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
 }
 
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-
-    &s[..]
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
-
