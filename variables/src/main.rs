e21d19861939f8721ca66171a1155d5d7b0e235a
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-03-30 07:03:26 +0900

    square追加

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -12,6 +12,10 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
