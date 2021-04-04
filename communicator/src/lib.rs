645216039f706f0e39b6c4eabdbdeacdecdd391c
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-04-05 07:00:53 +0900

    tmp

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -1,16 +1,5 @@
-mod client;
+pub mod client;
 mod network;
-
-mod network {
-    fn connect() {
-    }
-
-    mod server {
-        fn connect() {
-        }
-    }
-}
-
 
 #[cfg(test)]
 mod tests {
