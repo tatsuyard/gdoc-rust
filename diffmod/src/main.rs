7ba54305a5b71d9dc50dd253938c9ac532fdf1e3
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-04-09 06:39:24 +0900

    enum

--- diffmod/src/main.rs
+++ diffmod/src/main.rs
@@ -1,13 +1,13 @@
-pub mod a {
-    pub mod series {
-        pub mod of {
-            pub fn nested_modules() {}
-        }
-    }
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
 }
 
-use a::series::of;
+use TrafficLight::{Red, Yellow};
 
 fn main() {
-    of::nested_modules();
+    let red = Red;
+    let yellow = Yellow;
+    let green = TrafficLight::Green;
 }
