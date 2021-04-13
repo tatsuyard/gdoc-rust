a796fa027723adc5aed05583ee81ef05622c8e85
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-04-14 07:17:04 +0900

    tmp

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use std::env;
 use google_drive::GoogleDrive;
 use yup_oauth2::{read_service_account_key, ServiceAccountAuthenticator};
 
+async fn get_dirves() {
+
+}
+
 fn main() {
     println!("Hello World");
 }
