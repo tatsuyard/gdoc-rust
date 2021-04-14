9368bacdaaf5f2455da27a31a884d5142fcbfc3a
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-04-15 07:14:33 +0900

    tmp

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use google_drive::GoogleDrive;
 use yup_oauth2::{read_service_account_key, ServiceAccountAuthenticator};
 
 async fn get_dirves() {
-
+    let gsuite_credential_file =
+        env::var("GADMIN_CREDENTIAL_FILE").unwrap();
 }
 
 fn main() {
