5f26b2ff9a156c3268023c012e676e5f7be3692d
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-04-16 07:26:55 +0900

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+extern crate dotenv;
+use dotenv::dotenv;
 use std::env;
 use google_drive::GoogleDrive;
 use yup_oauth2::{read_service_account_key, ServiceAccountAuthenticator};
 
-async fn get_dirves() {
-    let gsuite_credential_file =
-        env::var("GADMIN_CREDENTIAL_FILE").unwrap();
-}
+// async fn get_dirves() {
+//     let gsuite_credential_file =
+//         env::var("GADMIN_CREDENTIAL_FILE").unwrap();
+// }
 
 fn main() {
-    println!("Hello World");
+    dotenv().ok();
+
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
 }
