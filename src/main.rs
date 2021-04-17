4965b45ed605fa2745c50d70709244e07d491752
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-04-18 07:23:06 +0900

    tmp

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,24 @@ use std::env;
 use google_drive::GoogleDrive;
 use yup_oauth2::{read_service_account_key, ServiceAccountAuthenticator};
 
-// async fn get_dirves() {
-//     let gsuite_credential_file =
-//         env::var("GADMIN_CREDENTIAL_FILE").unwrap();
-// }
+async fn get_dirves() {
+    let gsuite_credential_file =
+        env::var("GADMIN_CREDENTIAL_FILE").unwrap();
+    let gsuite_secret = read_service_account_key(gsuite_credential_file)
+        .await
+        .expect("failed to read gsuite credential file");
+    let auth = ServiceAccountAuthenticator::builder(gsuite_secret)
+        .subject(gsuite_subject.to_string())
+        .build()
+        .await
+        .expect("failed to create authenticator");
+}
 
 fn main() {
     dotenv().ok();
 
-    for (key, value) in env::vars() {
-        println!("{}: {}", key, value);
-    }
+    get_dirves();
+    // for (key, value) in env::vars() {
+    //     println!("{}: {}", key, value);
+    // }
 }
