37d13e79442082e9bc204f48b17e45ae60184a5f
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-04-19 07:08:26 +0900

    tmp

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,26 @@ async fn get_dirves() {
     let gsuite_secret = read_service_account_key(gsuite_credential_file)
         .await
         .expect("failed to read gsuite credential file");
+    let gsuite_subject = env::var("GADMIN_SUBJECT").unwrap();
     let auth = ServiceAccountAuthenticator::builder(gsuite_secret)
         .subject(gsuite_subject.to_string())
         .build()
         .await
         .expect("failed to create authenticator");
+    let token = auth.token(&["https://www.googleapis.com/auth/drive"])
+        .await
+        .expect("failed to get token");
+
+    if token.as_str().is_empty() {
+        panic!("empty token is not valid");
+    }
+
+    let drive_client = GoogleDrive::new(token);
+
+    let drives = drive_client.list_drives().await.unwrap();
+    for drive in drives {
+        println!("{:?}", drive);
+    }
 }
 
 fn main() {
