94b1dced38cc84cd435870d48a4b0b3a12e8e4f5
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-05-02 06:08:13 +0900

    関数の有効期間

--- diffmod/src/main.rs
+++ diffmod/src/main.rs
@@ -1,13 +1,15 @@
-enum TrafficLight {
-    Red,
-    Yellow,
-    Green,
+fn main() {
+    let magic1 = String::from("abracadabra!");
+    let magic2 = String::from("shazam!");
+
+    let result = longest_word(&magic1, &magic2);
+    println!("The longest magic word is {}", result);
 }
 
-use TrafficLight::*;
-
-fn main() {
-    let red = Red;
-    let yellow = Yellow;
-    let green = TrafficLight::Green;
+fn longest_word(x: &String, y: &String) -> &String {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
