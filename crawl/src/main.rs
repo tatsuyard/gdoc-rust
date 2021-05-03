43eec50b62840e9c10b7f9724c0a15f7fa1df288
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-05-04 06:04:45 +0900

    リンクを抽出

--- crawl/src/main.rs
+++ crawl/src/main.rs
@@ -1,7 +1,14 @@
+use select::document::Document;
+
 fn main() -> eyre::Result<()> {
     let body = reqwest::blocking::get("https://www.rust-lang.org")?
     .text()?;
 
-    println!("body = {:?}", body);
+    // println!("body = {:?}", body);
+    let doc = Document::from(body.as_str());
+    use select::predicate::Name;
+    for a in doc.find(Name("a")) {
+        println!("{:?}", a);
+    }
     Ok(())
 }
