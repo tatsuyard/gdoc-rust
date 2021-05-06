5747ad48cdbb630ec5f0bb6ef6aab64a64938ce4
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-05-07 07:08:46 +0900

    絶対URLで表示

--- crawl/src/main.rs
+++ crawl/src/main.rs
@@ -1,19 +1,21 @@
 use select::document::Document;
 
 fn main() -> eyre::Result<()> {
-    let body = reqwest::blocking::get("https://www.rust-lang.org")?
-    .text()?;
-
+    let response = reqwest::blocking::get("https://www.rust-lang.org")?;
+    let base_url = response.url().clone();
+    let body = response.text()?;
     // println!("body = {:?}", body);
     let doc = Document::from(body.as_str());
     use select::predicate::Name;
     use url::Url;
     for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
+        use url::ParseError as UrlParseError;
         // println!("{:?}", Url::parse(href));
         match Url::parse(href) {
             Ok(url) => { println!("{}", url); },
             Err(UrlParseError::RelativeUrlWithoutBase) => {
-
+                let url = base_url.join(href)?;
+                println!("{}", url);
             },
             Err(e) => {},
         }
