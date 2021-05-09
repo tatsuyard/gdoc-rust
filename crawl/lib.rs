25bd6db6b795b67138d3ab47f11a74ebf3557f0a
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-05-10 07:07:20 +0900

    tmp

--- crawl/lib.rs
+++ crawl/lib.rs
@@ -3,3 +3,11 @@ use reqwest::blocking::Client;
 pub struct LinkExtractor {
     client: Client,
 }
+
+impl LinkExtractor {
+    pub fn from_client(client: Client) -> Self {
+        Self {
+            client: client,
+        }
+    }
+}
