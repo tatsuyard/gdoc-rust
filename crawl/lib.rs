857e18bf1ed1610547ddbd29a694e409b0885308
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-05-12 07:07:50 +0900

    tmp

---
+++ crawl/lib.rs
@@ -0,0 +1,18 @@
+use reqwest::blocking::Client;
+
+pub struct LinkExtractor {
+    client: Client,
+}
+
+impl LinkExtractor {
+    pub fn from_client(client: Client) -> Self {
+        Self {
+            client: client,
+        }
+    }
+}
+
+pub fn get_links(&self, url: Url) -> Reault<Vec<Url>, eyre::Report> {
+    let reponse = self.client.get(url)?;
+    let base_url = response.url().clone();
+}
