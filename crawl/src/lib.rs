9e5be8649cd30eeb4cace7513fb36fe29fdc36b3
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-05-15 07:09:01 +0900

    tmp

--- crawl/src/lib.rs
+++ crawl/src/lib.rs
@@ -13,7 +13,7 @@ impl LinkExtractor {
 }
 
 pub fn get_links(&self, url: Url) -> Reault<Vec<Url>, eyre::Report> {
-    let reponse = self.client.get(url)?;
+    let reponse = self.client.get(url).send()?;
     let base_url = response.url().clone();
     let body = response.text()?;
     let doc = Document::from(body.as_str());
