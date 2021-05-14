488c871d784d1fdd2142dda8deb5f62170931174
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-05-15 07:07:17 +0900

    tmp

--- crawl/src/lib.rs
+++ crawl/src/lib.rs
@@ -18,4 +18,18 @@ pub fn get_links(&self, url: Url) -> Reault<Vec<Url>, eyre::Report> {
     let body = response.text()?;
     let doc = Document::from(body.as_str());
     let mut links = Vec::new();
+    use url::Url;
+    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
+        use url::ParseError as UrlParseError;
+        // println!("{:?}", Url::parse(href));
+        match Url::parse(href) {
+            Ok(url) => { println!("{}", url); },
+            Err(UrlParseError::RelativeUrlWithoutBase) => {
+                let url = base_url.join(href)?;
+                println!("{}", url);
+            },
+            Err(e) => {},
+        }
+    }
+    Ok(())
 }
