480a519b477c0a0d7b5f59f575d1d12a29e64bcf
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-05-18 07:03:19 +0900

    場所が間違っていたので修正

--- crawl/src/lib.rs
+++ crawl/src/lib.rs
@@ -12,27 +12,27 @@ impl LinkExtractor {
             client: client,
         }
     }
-}
-
-pub fn get_links(&self, url: Url) -> Result<Vec<Url>, eyre::Report> {
-    let response = self.client.get(url).send()?;
-    let base_url = response.url().clone();
-    let body = response.text()?;
-    let doc = Document::from(body.as_str());
-    use select::predicate::Name;
-    let mut links = Vec::new();
     
-    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
-        use url::ParseError as UrlParseError;
-        // println!("{:?}", Url::parse(href));
-        match Url::parse(href) {
-            Ok(url) => { println!("{}", url); },
-            Err(UrlParseError::RelativeUrlWithoutBase) => {
-                let url = base_url.join(href)?;
-                println!("{}", url);
-            },
-            Err(e) => {},
+    pub fn get_links(&self, url: Url) -> Result<Vec<Url>, eyre::Report> {
+        let response = self.client.get(url).send()?;
+        let base_url = response.url().clone();
+        let body = response.text()?;
+        let doc = Document::from(body.as_str());
+        use select::predicate::Name;
+        let mut links = Vec::new();
+        
+        for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
+            use url::ParseError as UrlParseError;
+            // println!("{:?}", Url::parse(href));
+            match Url::parse(href) {
+                Ok(url) => { println!("{}", url); },
+                Err(UrlParseError::RelativeUrlWithoutBase) => {
+                    let url = base_url.join(href)?;
+                    println!("{}", url);
+                },
+                Err(e) => {},
+            }
         }
+        Ok(links)
     }
-    Ok(links)
 }
