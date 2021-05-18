0b04089f1c4dc8997a0d3443f9b9bcb8d0f62c4f
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-05-19 07:23:14 +0900

    tmp

--- crawl/src/main.rs
+++ crawl/src/main.rs
@@ -1,13 +1,13 @@
 use reqwest::blocking::ClientBuilder;
 use url::Url;
-use:crawler::LinkExtractor;
+use crawl::LinkExtractor;
 
 fn main() -> eyre::Result<()> {
     let url = Url::parse("https://www.rust-lang.org")?;
     let client = ClientBuilder::new().build()?;
     let extractor = LinkExtractor::from_client(client);
     let links = extractor.get_links(url)?;
-    for links in links.iter() {
+    for link in links.iter() {
         println!("{}", link);
     }
     
