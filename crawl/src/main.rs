9b543178fb819f9867ee0d772de390b7b4d2c267
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-05-23 06:04:26 +0900

    log追加

--- crawl/src/main.rs
+++ crawl/src/main.rs
@@ -2,7 +2,9 @@ use reqwest::blocking::ClientBuilder;
 use url::Url;
 use crawl::LinkExtractor;
 
+
 fn main() -> eyre::Result<()> {
+    env_logger::init();
     let url = Url::parse("https://www.rust-lang.org")?;
     let client = ClientBuilder::new().build()?;
     let extractor = LinkExtractor::from_client(client);
