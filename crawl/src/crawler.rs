5ce799185d81936cfdf8cb541fdf1ac66034bf9d
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-06-22 07:30:37 +0900

    tmp

--- crawl/src/crawler.rs
+++ crawl/src/crawler.rs
@@ -32,6 +32,6 @@ G: AdjacentNodes,
 <G as AdjacentNodes>::Node: Clone + Hash + Eq + Borrow<<G as AdjacentNodes>::Node>,
 {
     pub fn new(graph: &'a G, start: <G as AdjacentNodes>::Node) -> Self {
-        
+        let mut visit = VecDeque::new();
     }
 }
