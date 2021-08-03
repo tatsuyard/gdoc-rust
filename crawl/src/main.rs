ec99e7604304e789fda54d3645cb3369a00d272d
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-08-04 06:57:40 +0900

    tmp

--- crawl/src/main.rs
+++ crawl/src/main.rs
@@ -1,23 +1,24 @@
+use crawl::LinkExtractor;
 use reqwest::blocking::ClientBuilder;
 use url::Url;
-use crawl::LinkExtractor;
-
 
 fn main() -> eyre::Result<()> {
     env_logger::init();
-    
+
     let url = std::env::args()
-    .nth(1)
-    .unwrap_or("https://www.rust-lang.org".to_owned());
+        .nth(1)
+        .unwrap_or("https://www.rust-lang.org".to_owned());
 
     let url = Url::parse(&url)?;
     let client = ClientBuilder::new().build()?;
     let extractor = LinkExtractor::from_client(client);
-    
+
+    let wait = Duration::from_mills(100);
+
     let links = extractor.get_links(url)?;
     for link in links.iter() {
         println!("{}", link);
     }
-    
+
     Ok(())
 }
