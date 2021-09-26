4dd5c435ebf7a8bcc7555b03623f06ee60ce3392
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-09-27 07:35:39 +0900

    tmp

--- crawl/src/crawler.rs
+++ crawl/src/crawler.rs
@@ -35,7 +35,7 @@ where
 
     #[test]
     fn bfs0() {
-        let graph = AdjVec(vec![vec![1, 2], vec![0, 3], vec![3]]);
+        let graph = AdjVec(vec![vec![1, 2], vec![0, 3], vec![3], vec![2, 0]]);
     }
 }
 
