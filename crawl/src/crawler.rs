5b602d86dccf90b10ff8c2c55da5e54379622112
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2021-11-21 10:17:48 +0900

    tmp

--- crawl/src/crawler.rs
+++ crawl/src/crawler.rs
@@ -117,7 +117,12 @@ fn bfs() {
 
     #[test]
     fn bfs3() {
-        let graph = AdjVec(vec![vec![1, 1, 2], vec![2, 3], vec![], vec![]]);
+        let graph = AdjVec(vec![
+            vec![1, 1, 2], 
+            vec![2, 3], 
+            vec![], 
+            vec![]
+        ]);
         let crawler = Crawler::new(&graph, 0);
         let nodes: Vec<usize> = crawler.collect();
         assert_eq!(nodes, vec![0, 1, 2, 3]);
