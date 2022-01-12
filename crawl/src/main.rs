bbaef1ecc60a5105f0ce4feebc56ff23e2ac102a
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-01-12 23:34:49 +0900

    tmp

--- crawl/src/main.rs
+++ crawl/src/main.rs
@@ -21,6 +21,7 @@ fn main() -> eyre::Result<()> {
     let extractor = LinkExtractor::from_client(client);
 
     let wait = Duration::from_mills(100);
+    
     for url in crawler.take(opt.maximum_pages) {
         println!("{}", url);
         std::thread::sleep(wait.clone());
