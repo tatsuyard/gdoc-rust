9315d894620c113037f44378eff6ad2442258d1a
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-01-19 08:43:53 +0900

    tmp

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -3,6 +3,5 @@ mod cli;
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
-
     println!("{:?}, {:?}", action, item);
 }
