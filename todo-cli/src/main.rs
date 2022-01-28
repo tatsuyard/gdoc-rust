d56e1b7e78677d810fcb0b320fc1787167a0b819
TatsuyaKamikawara <tatsuyardful@gmail.com>
2022-01-29 08:12:34 +0900

    tmp

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,7 +1,6 @@
 mod cli;
+use structopt::StructOpt;
 
 fn main() {
-    let action = std::env::args().nth(1).expect("Please specify an action");
-    let item = std::env::args().nth(2).expect("Please specify an item");
-    println!("{:?}, {:?}", action, item);
+    cli::CommandLineArgs::from_args();
 }
