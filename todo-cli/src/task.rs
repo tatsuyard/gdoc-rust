570a9c4bd363cecc5fe9bc95e27df1f55bbe1742
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-02-18 08:41:29 +0900

    tmp

--- todo-cli/src/task.rs
+++ todo-cli/src/task.rs
@@ -5,6 +5,8 @@ use serde::Serialize;
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
+
+    #[serde(with = "ts_seconds")]
     pub created_at: DateTime<Utc>,
 }
 
