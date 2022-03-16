282bf7fbb78d4f3052624b95ba8cd7e3823bd2cd
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-03-17 08:29:32 +0900

    tmp

--- todo-cli/src/task.rs
+++ todo-cli/src/task.rs
@@ -45,5 +45,4 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
 }
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {}
-
 pub fn list_tasks(journal_path: PathBuf) -> Result<()> {}
