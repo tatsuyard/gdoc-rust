4451d8044f61eb804cd51ebc835c2f8a82b907c0
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-04-15 08:15:02 +0900

    tmp

--- todo-cli/src/task.rs
+++ todo-cli/src/task.rs
@@ -22,18 +22,14 @@ impl Task {
 
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
     // Open the file.
-    let mut file = OpenOptions::new()
+    let file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
         .open(journal_path)?;
 
     // Consume the file's contents as a vector of tasks.
-    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
-        Ok(tasks) => tasks,
-        Err(e) if e.is_eof() => Vec::new(),
-        Err(e) => Err(e)?,
-    };
+    let mut tasks = collect_tasks(&file)?;
     // Rewind the file after reading from it.
     file.seek(SeekForm::Start(0))?;
 
