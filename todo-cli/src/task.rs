29dc7e8d3e9e7a22938777977918582d95996c82
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-04-16 10:37:31 +0900

    tmp

--- todo-cli/src/task.rs
+++ todo-cli/src/task.rs
@@ -30,9 +30,6 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
 
     // Consume the file's contents as a vector of tasks.
     let mut tasks = collect_tasks(&file)?;
-    // Rewind the file after reading from it.
-    file.seek(SeekForm::Start(0))?;
-
     // Write the modified task list back into the file.
     tasks.push(task);
     serde_json::to_writer(file, &tasks);
