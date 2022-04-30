79f90bea31e9567fd8d039d9dcb0814a47e11038
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-04-30 09:56:13 +0900

    tmp

--- todo-cli/src/task.rs
+++ todo-cli/src/task.rs
@@ -60,6 +60,7 @@ pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
         let mut order: u32 = 1;
         for task in tasks {
             println!("{}: {}", order, task);
+            order += 1;
         }
     }
 }
