bc5d4aad47b91e8b34d6b444b880656e61b42db1
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-05-11 08:23:53 +0900

    tmp

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -6,5 +6,6 @@ use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
 
 fn main() {
+    let CommandLineArgs {} = CommandLineArgs::from_args();
     println!("{:#?}", cli::CommandLineArgs::from_args());
 }
