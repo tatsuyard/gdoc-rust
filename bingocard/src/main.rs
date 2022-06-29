9bedb281a00128b503bc2d22b05a2157b46c0600
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-06-30 07:06:54 +0900

    tmp

--- bingocard/src/main.rs
+++ bingocard/src/main.rs
@@ -2,7 +2,7 @@ use rand::seq::SliceRandom;
 
 fn main() {
     let mut nums = [0; 75];
-    for i in ..=75 {
+    for i in 1..=75 {
         nums[i - 1] = i;
     }
     let mut rng = rand::thread_rng();
