6846c541f41a0f64335ad410c1b01642e3b929ce
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-07-04 07:25:40 +0900

    tmp

--- bingocard/src/main.rs
+++ bingocard/src/main.rs
@@ -17,16 +17,4 @@ fn main() {
             println!("");
         }
     }
-
-    for y in 0..5 {
-        for x in 0..5 {
-            let i = y * 5 + x;
-            if i == 12 {
-                print!(" *,");
-            } else {
-                print!("{:3},", nums[i]);
-            }
-        }
-        println!("");
-    }
 }
