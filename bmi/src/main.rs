bdb366fca49d2727e3fca23874ef28fc0099c6c4
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-07-09 07:49:42 +0900

    tmp

--- bmi/src/main.rs
+++ bmi/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let height_cm = input("身長(cm)は？");
+    let weight = input("体重(kg)は？");
 }
 
-fn input(prompt: &str) -> f64 {}
+fn input(prompt: &str) -> f64 {
+    println!("{}", prompt);
+}
