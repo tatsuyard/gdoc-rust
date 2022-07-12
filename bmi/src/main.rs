b34f051a50f781fa0299eaa5e37539e9b96f0899
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-07-13 07:32:17 +0900

    tmp

--- bmi/src/main.rs
+++ bmi/src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     let height = height_cm / 100.0;
     let bmi = weight / height.powf(2.0);
     println!("BMI={:.1}", bmi);
+    if bmi < 18.5 {
+        println!("低体重");
+    }
 }
 
 fn input(prompt: &str) -> f64 {
