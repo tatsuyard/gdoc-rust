297018be0bc772616778f495fc6e113a7678977a
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-08-10 08:12:55 +0900

    tmp

--- bmi/src/input_height.rs
+++ bmi/src/input_height.rs
@@ -20,8 +20,5 @@ fn input_str() -> String {
 
 fn input_f(def: f64) -> f64 {
     let s = input_str();
-    match s.trim().parse() {
-        Ok(v) => v,
-        Err(_) => def,
-    }
+    s.trim().parse().unwrap()
 }
