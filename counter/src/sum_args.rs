c510729379032d0dfc9803d21b82b8f9e946096b
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-09-15 08:10:54 +0900

    tmp

--- counter/src/sum_args.rs
+++ counter/src/sum_args.rs
@@ -6,5 +6,9 @@ fn main() {
         if i == 0 {
             continue;
         }
+        let num: f64 = match s.parse() {
+            Ok(v) => v,
+            Err(_) => 0.0,
+        };
     }
 }
