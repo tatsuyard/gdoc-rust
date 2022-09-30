7b66783304602cb7c16d8fb1bde2df687ceeb501
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-10-01 08:53:38 +0900

    tmp

--- counter/src/sum_textfile.rs
+++ counter/src/sum_textfile.rs
@@ -3,5 +3,10 @@ use std::{env, fs};
 fn main() {
     let args = env::args();
     let mut total: f64 = 0.0;
-    for (i, fname) in args.enumerate() {}
+    for (i, fname) in args.enumerate() {
+        if i == 0 {
+            continue;
+        }
+        let text = fs::read_to_string(fname).unwrap();
+    }
 }
