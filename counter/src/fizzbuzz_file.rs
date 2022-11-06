3aaa038f7154f7bfeeca2bf674973754d52e97b1
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-11-07 08:06:43 +0900

    tmp

--- counter/src/fizzbuzz_file.rs
+++ counter/src/fizzbuzz_file.rs
@@ -6,6 +6,7 @@ fn main() {
     {
         let fp = file::create(filename).unwrap();
         let mut writer = BufWriter::new(fp);
+
         for i in 1..=100 {
             let mut line = format!("{}\n", i);
             if (i % 3 == 0) && (i % 5 == 0) {
