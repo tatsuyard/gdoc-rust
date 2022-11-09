328a815ce998cdf1b80e6df1cf8471b58aef609e
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-11-10 08:08:48 +0900

    tmp

--- counter/src/fizzbuzz_file2.rs
+++ counter/src/fizzbuzz_file2.rs
@@ -1,2 +1,9 @@
 use std::fs::File;
 use std::io::Write;
+
+fn main() {
+    let filename = "fizzbuzz_file2_result.txt";
+    let data = get_fizzbuzz(100);
+
+    fn get_fizzbuzz(max: u32) -> String {}
+}
