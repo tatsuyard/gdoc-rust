7f3edcc8c4852dd4177624c393605117df21ea82
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-11-25 08:14:53 +0900

    tmp

--- counter/src/rec_sum1to10.rs
+++ counter/src/rec_sum1to10.rs
@@ -5,6 +5,19 @@ fn sum(n: i32) -> i32 {
     return sum(n - 1) + n;
 }
 
+fn fib(n: i64) -> i64 {
+    if n == 1 {
+        return 0;
+    }
+    if n == 2 {
+        return 1;
+    }
+    return fib(n - 2) + fib(n - 1);
+}
+
 fn main() {
     println!("{}", sum(10));
+    for i in 2..=20 {
+        println!("{}", fib(i));
+    }
 }
