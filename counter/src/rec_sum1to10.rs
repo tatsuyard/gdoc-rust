330b6815f46dc3e79970b0eb0f1a7483bdf244ad
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-11-28 08:05:12 +0900

    tmp

--- counter/src/rec_sum1to10.rs
+++ counter/src/rec_sum1to10.rs
@@ -2,4 +2,8 @@ use std::{env, path};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("findfile (path) (keyword)");
+        return;
+    }
 }
