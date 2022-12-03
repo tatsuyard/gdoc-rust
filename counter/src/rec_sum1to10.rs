7fbcdcdacaefe50954b219743517b8675f758485
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-12-03 09:27:32 +0900

    tmp

--- counter/src/rec_sum1to10.rs
+++ counter/src/rec_sum1to10.rs
@@ -11,3 +11,7 @@ fn main() {
     let target = path::PathBuf::from(target_dir);
     findfile(&target, keyword);
 }
+
+fn findfile(target: &path::PathBuf, keyword: &str) {
+    let files = target.read_dir().expect("存在しないパス");
+}
