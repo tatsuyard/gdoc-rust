7b3a075e18128405c1e47ce2b1d1c421a510e246
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-12-13 08:09:28 +0900

    tmp

--- counter/src/cur_files.rs
+++ counter/src/cur_files.rs
@@ -4,6 +4,7 @@ fn main() {
     let files = fs::read_dir(".").expect("不正なパス");
     for ent in files {
         let entry = ent.unwrap();
-        let path = ent.path();
+        let path = entry.path();
+        let fname = path.to_str().unwrap_or("不正なファイル名");
     }
 }
