14624688b862806298f3bc5ed575915110ce64cc
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2022-12-16 08:13:56 +0900

    tmp

--- counter/src/mytree.rs
+++ counter/src/mytree.rs
@@ -2,4 +2,8 @@ use std::{env, path};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut target_dir = ".";
+    if args.len() >= 2 {
+        target_dir = &args[1];
+    }
+    let target = path::PathBuf::from(target_dir);
 }
