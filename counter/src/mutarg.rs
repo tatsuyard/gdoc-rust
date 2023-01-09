29b5693e45678780d8806d10936e0076e6ffdd0f
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-01-10 08:30:14 +0900

    tmp

--- counter/src/mutarg.rs
+++ counter/src/mutarg.rs
@@ -1,9 +1,14 @@
 fn x2(arg: &mut i32) {
     *arg = *arg * 2;
 }
-
+fn greet(msg: &String) {
+    println!("{}", msg);
+}
 fn main() {
     let mut v = 16;
     x2(&mut v);
+
+    let msg = String::from("hoge");
+    greet(&msg);
     println!("{}", v);
 }
