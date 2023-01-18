6db533a4a11979f700a94ea8c3ffa12a33a0d65e
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-01-19 08:27:53 +0900

    tmp

--- counter/src/tuple_goods.rs
+++ counter/src/tuple_goods.rs
@@ -2,6 +2,9 @@ fn main() {
     let banana = ("バナナ", 300);
     let apple = ("リンゴ", 200);
     let total = banana.1 + apple.1;
+    print_tuple(&banana);
+    print_tuple(&apple);
+    println!("合計{}円です", total);
 }
 
 fn print_tuple(item: &(&str, i64)) {
