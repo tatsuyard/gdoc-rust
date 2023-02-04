64236a25c2afba10493a13efa7b295a2d68da65c
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-02-04 09:49:38 +0900

    tmp

--- counter/src/slice_str.rs
+++ counter/src/slice_str.rs
@@ -10,5 +10,5 @@ fn main() {
     let a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     println!("a={}", sum_slice(&a[..]));
     let b = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    println!("{:?}", &a[3..6]);
+    println!("b={}", sum_slice(&b[..]));
 }
