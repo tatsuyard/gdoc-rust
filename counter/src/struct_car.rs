7f98b9c7912bed66e839dded87f6b1ba4ced9135
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-02-05 08:54:54 +0900

    tmp

---
+++ counter/src/struct_car.rs
@@ -0,0 +1,13 @@
+struct CarSpec {
+    model: i32,
+    cc: i32,
+    color: i32,
+}
+
+fn main() {
+    let car1 = CarSpec {
+        model: 3001,
+        cc: 1500,
+        color: 0xFF0000,
+    };
+}
