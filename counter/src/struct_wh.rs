c7e38fc6f90ec683e843ab9565eca322b87c04b7
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-02-11 09:41:24 +0900

    tmp

--- counter/src/struct_wh.rs
+++ counter/src/struct_wh.rs
@@ -8,4 +8,13 @@ fn main() {
         weight: 80.0,
         height: 154.0,
     };
+    let jiro = Body {
+        weight: 65.0,
+        height: 170.0,
+    };
+}
+
+fn calc_bmi(body: &body) -> f64 {
+    let h = body.height / 100.0;
+    body.weight / h.powf(2.0);
 }
