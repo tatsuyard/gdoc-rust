bfdbefba4099673731edd649e4673b3fce40d5d9
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-03-05 09:49:38 +0900

    tmp

--- counter/src/bmi_struct.rs
+++ counter/src/bmi_struct.rs
@@ -9,36 +9,36 @@ fn main() {
     let weight = input("体重(kg)は？");
     let height = height_cm / 100.0;
     let bmi = weight / height.powf(2.0);
-    let bmi_list = Vec![
+    let bmi_list = vec![
         BmiRange {
             min: 0.0,
             max: 18.5,
-            label: "低体重"
+            label: "低体重",
         },
         BmiRange {
             min: 18.5,
             max: 25.0,
-            label: "普通体重"
+            label: "普通体重",
         },
         BmiRange {
             min: 25.0,
             max: 30.0,
-            label: "肥満1度"
+            label: "肥満1度",
         },
         BmiRange {
             min: 30.0,
             max: 35.0,
-            label: "肥満2度"
+            label: "肥満2度",
         },
         BmiRange {
             min: 35.0,
             max: 40.0,
-            label: "肥満3度"
+            label: "肥満3度",
         },
         BmiRange {
             min: 45.0,
             max: 99.0,
-            label: "肥満4度"
+            label: "肥満4度",
         },
     ];
     let mut result = "不明";
@@ -55,5 +55,5 @@ fn input(prompt: &str) -> f64 {
     println!("{}", prompt);
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).expect("入力エラー");
-    s.trim().parse().expect("数値変換エラーです");
+    s.trim().parse().expect("数値変換エラーです")
 }
