20a9af44f4414e66929bd69f85a2e573c170a34a
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-03-09 08:20:00 +0900

    tmp

--- counter/src/str_index.rs
+++ counter/src/str_index.rs
@@ -2,5 +2,6 @@ fn main() {
     let s = "Hello";
     let ch = s.chars().nth(0).unwrap();
     println!("{}", ch);
-    let ch = s,chars().nth(2).unwrap();
+    let ch = s.chars().nth(2).unwrap();
+    println!("{}", ch);
 }
