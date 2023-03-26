b2a82cd1eec404200ccb6e689f4a1f01bb7bdbc5
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-03-27 06:42:05 +0900

    tmp

--- counter/src/str_tochars.rs
+++ counter/src/str_tochars.rs
@@ -3,15 +3,6 @@ fn echo(s: &'static str) {
 }
 
 fn main() {
-    let pr = "窮鼠猫を噛む";
-    for c in pr.chars() {
-        print!("[{}]", c);
-    }
-    println!("\n文字数={}字", pr.chars().count());
-
-    let pr_chars: Vec<char> = pr.chars().collect();
-    for c in pr_chars() {
-        print!("({})", c);
-    }
-    println!("\n文字数={}字", pr_chars.len());
+    echo("愚かな人でも黙っていると");
+    echo("賢いと見られる");
 }
