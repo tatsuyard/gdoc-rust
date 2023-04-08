21ec409e434167ce62efb8e1c0b65ea75a3ad49c
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-04-09 08:50:07 +0900

    tmp

--- counter/src/str_substr.rs
+++ counter/src/str_substr.rs
@@ -11,4 +11,9 @@ fn main() {
     }
     println!("先頭2文字: {}", sub1);
     let mut sub2 = String::new();
+    for (i, c) in pr.chars().enumerate() {
+        if 3 <= i && i <= 4 {
+            sub2.push(c);
+        }
+    }
 }
