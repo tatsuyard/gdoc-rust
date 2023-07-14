b5112c40d743efd205390528eba05a8166dc5745
Tatsuta Kamikawara <tatsuyardful@gmail.com>
2023-07-14 11:48:37 +0900

    tmp

--- counter/src/main.rs
+++ counter/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
         let processes = system.get_processes();
         let mut sorted_processes: Vec<_> = processes.values().collect();
-        sorted_processes.sort_by_key(|p| Reverse(p.cpu_usage()));
+        sorted_processes.sort_by(|a, b| b.cpu_usage().partial_cmp(&a.cpu_usage()).unwrap());
 
         println!("PID\tCPU%\tMemory\tName");
         for process in sorted_processes.iter().take(10) {
